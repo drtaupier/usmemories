ea3e678dbaa45e5224449143728653ad1c6e80e0
drtaupier <drtaupier@gmail.com>
2020-04-26 15:28:18 -0400

    I did json file about the courses information

---
+++ src/client/js/json/precios.js
@@ -0,0 +1,17 @@
+let cursos = [
+    {
+    "carrera" : "Marketing",
+    "informes" : "Informes",
+    "costo" : "19.99"
+    },
+    {
+    "carrera" : "E-Commerce",
+    "informes" : "Informes",
+    "costo" : "19.99"
+    },
+    {
+    "carrera" : "Marketing",
+    "informes" : "Informes",
+    "costo" : "19.99"
+    }
+]
